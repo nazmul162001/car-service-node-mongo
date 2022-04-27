54365727618e2d9cdb6f3c53dc0def9f610935dd
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-27 15:31:46 +0600

    update orders for server

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,31 +1,35 @@
-import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axiosPrivet';
 import auth from '../../firebase.init';
 
 const Orders = () => {
+  const navigate = useNavigate();
   const [orders, setOrders] = useState([]);
   const [user] = useAuthState(auth);
   console.log(orders);
 
   useEffect(() => {
     //----- here you can use fetch --------//
-    const getOrders = async() => {
+    const getOrders = async () => {
       const email = user?.email;
       const url = `http://localhost:5000/order?email=${email}`;
-      const {data} = await axios.get(url, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem('accessToken')}`
+      try {
+        const { data } = await axiosPrivate.get(url);
+        setOrders(data);
+      } catch (error) {
+        console.log(error.message);
+        if(error.response.status === 401 || error.response.status === 403) {
+          signOut(auth)
+          navigate('/login')
         }
-      });
-      setOrders(data);
-    }
+      }
+    };
     getOrders();
-    
-  },[])
-  
-  
-  
+  }, []);
+
   return (
     <div>
       <h2>This is my orders {orders.length} </h2>
